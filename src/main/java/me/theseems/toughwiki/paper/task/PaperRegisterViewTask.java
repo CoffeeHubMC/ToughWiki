b3fd9b34a389317fa1d5bc1a86de9ad669e96a6b
TheSeems <me@theseems.ru>
2022-10-03 02:17:20 +0300

    Remove mandatory IFWikiPageView for every page there is

--- src/main/java/me/theseems/toughwiki/paper/task/PaperRegisterViewTask.java
+++ src/main/java/me/theseems/toughwiki/paper/task/PaperRegisterViewTask.java
@@ -1,11 +1,13 @@
 package me.theseems.toughwiki.paper.task;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import me.theseems.toughwiki.api.ToughWikiAPI;
 import me.theseems.toughwiki.api.WikiPage;
 import me.theseems.toughwiki.impl.bootstrap.BootstrapTask;
 import me.theseems.toughwiki.impl.bootstrap.Phase;
 import me.theseems.toughwiki.paper.view.IFWikiPageView;
 
+import java.util.Optional;
 import java.util.logging.Logger;
 
 public class PaperRegisterViewTask extends BootstrapTask {
@@ -16,8 +18,20 @@ public class PaperRegisterViewTask extends BootstrapTask {
     @Override
     public void run(Logger logger) {
         for (WikiPage page : ToughWikiAPI.getInstance().getPageRepository().getAllPages()) {
-            IFWikiPageView view = new IFWikiPageView(page.getName(), page.getInfo().getModifiers());
-            ToughWikiAPI.getInstance().getViewManager().store(view);
+            boolean hasNoPendingView = getViewType(page)
+                    .map(text -> text.equals("default"))
+                    .orElse(true);
+            if (hasNoPendingView) {
+                IFWikiPageView view = new IFWikiPageView(page.getName(), page.getInfo().getModifiers());
+                ToughWikiAPI.getInstance().getViewManager().store(view);
+            }
         }
+    }
+
+    public static Optional<String> getViewType(WikiPage page) {
+        return Optional.ofNullable(page.getInfo().getModifiers())
+                .map(modifiers -> modifiers.get("viewType"))
+                .filter(JsonNode::isTextual)
+                .map(JsonNode::asText);
     }
 }
